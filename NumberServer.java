1fcecdf19d90ea3b87829822bb1a8c2eb1d76286
Haesol Jung <110417501+haesoljung04@users.noreply.github.com>
2023-01-26 15:23:44 -0800

    test

--- NumberServer.java
+++ NumberServer.java
@@ -1,5 +1,6 @@
 import java.io.IOException;
 import java.net.URI;
+import java.util.ArrayList;
 
 class Handler implements URLHandler {
     // The one bit of state on the server: a number that will be manipulated by
@@ -7,21 +8,15 @@ class Handler implements URLHandler {
     int num = 0;
 
     public String handleRequest(URI url) {
-        if (url.getPath().equals("/")) {
-            return String.format("Haesol's Number: %d", num);
-        } else if (url.getPath().equals("/increment")) {
-            num += 1;
-            return String.format("Number incremented!");
-        } else {
-            System.out.println("Path: " + url.getPath());
-            if (url.getPath().contains("/add")) {
-                String[] parameters = url.getQuery().split("=");
-                if (parameters[0].equals("count")) {
-                    num += Integer.parseInt(parameters[1]);
-                    return String.format("Number increased by %s! It's now %d", parameters[1], num);
-                }
-            }
-            return "404 Not Found!";
+        ArrayList<String> messages = new ArrayList();
+        if (url.getPath().equals("/add-message")) {
+            String[] parameters = url.getQuery().split("=");
+            messages.add(parameters[1]);
+            return messages.toString();
+
+        }
+        else {
+            return "404 not Found";
         }
     }
 }
